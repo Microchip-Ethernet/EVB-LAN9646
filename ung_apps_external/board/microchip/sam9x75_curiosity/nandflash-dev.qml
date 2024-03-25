6cf9752056122cd89f1affeeb6343935dfef2aa1
Tristram Ha <tristram.ha@microchip.com>
2024-03-24 21:28:18 -0700

    Add sam9x75_curiosity configurations.

---
+++ ung_apps_external/board/microchip/sam9x75_curiosity/nandflash-dev.qml
@@ -0,0 +1,37 @@
+import SAMBA 3.7
+import SAMBA.Connection.Serial 3.7
+import SAMBA.Device.SAM9X7 3.7
+
+SerialConnection {
+	device: SAM9X7 {
+		config {
+			nandflash {
+				ioset: 1
+				busWidth: 8
+				header: 0xc0914da5
+			}
+		}
+	}
+
+	onConnectionOpened: {
+		// change CPU_CLK/MCK frequencies to 600/200 MHz
+//		initializeApplet("lowlevel")
+
+		// initialize external DDR2
+//		initializeApplet("extram")
+
+		// initialize NAND flash applet
+		initializeApplet("nandflash")
+
+		// erase all memory
+		applet.erase(0, applet.memorySize)
+
+		// write files
+		applet.write(0x000000, "at91bootstrap.bin", true)
+		applet.write(0x040000, "u-boot.bin")
+		applet.write(0x100000, "uboot-env.bin")
+		applet.write(0x140000, "at91-sam9x75_curiosity_dev.dtb")
+		applet.write(0x180000, "zImage")
+		applet.write(0x780000, "rootfs.ubi")
+	}
+}
---
+++ ung_apps_external/board/microchip/sam9x75_curiosity/post-image.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+DTB=$(grep BR2_LINUX_KERNEL_INTREE_DTS_NAME $BR2_CONFIG | cut -d"\"" -f2)
+
+if [ "$DTB" != "at91-sam9x75_curiosity" ]; then
+    cp -p ${BINARIES_DIR}/$DTB.dtb ${BINARIES_DIR}/at91-sam9x75_curiosity.dtb
+fi
+
+# Not building for nandflash
+if [ ! -e ${BINARIES_DIR}/rootfs.ubi ]; then
+    ENV_IN=${BR2_EXTERNAL_KSZ_PATH}/u-boot-mmc.txt
+    ENV_OUT=${BINARIES_DIR}/uboot.env
+    ENV_SIZE=0x4000
+    ENV_REDUN=
+else
+    ENV_IN=${BR2_EXTERNAL_KSZ_PATH}/u-boot-nand.txt
+    ENV_OUT=${BINARIES_DIR}/uboot-env.bin
+    ENV_SIZE=0x20000
+    ENV_REDUN=-r
+fi
+
+if [ ! -e ${BINARIES_DIR}/rootfs.ubi ]; then
+    if [ ! -e ${ENV_OUT} ]; then
+        cp -p ${BINARIES_DIR}/uboot-env.bin ${ENV_OUT}
+    fi
+    BOARD_DIR="$(dirname $0)"
+    ./support/scripts/genimage.sh -c ${BOARD_DIR}/genimage.cfg
+else
+echo "./host/bin/sam-ba -w images -x ${BR2_EXTERNAL_KSZ_PATH}/board/microchip/sam9x75_curiosity/nandflash-dev.qml" > ${BASE_DIR}/flash.sh
+chmod +x ${BASE_DIR}/flash.sh
+fi
