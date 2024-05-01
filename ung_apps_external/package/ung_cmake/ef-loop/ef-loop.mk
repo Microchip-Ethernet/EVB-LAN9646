7e38b646452e707d445f93f8c420bb1be5e33792
Tristram Ha <tristram.ha@microchip.com>
2024-05-01 14:39:20 -0700

    Add easyframes, easyrun, and ef-loop utilities in ung_apps_external.

---
+++ ung_apps_external/package/ung_cmake/ef-loop/ef-loop.mk
@@ -0,0 +1,14 @@
+################################################################################
+#
+# ef-loop
+#
+################################################################################
+
+EF_LOOP_VERSION = 0
+EF_LOOP_SITE = $(BR2_EXTERNAL_KSZ_PATH)/cmake_apps/ef-loop
+EF_LOOP_SITE_METHOD = local
+EF_LOOP_DEPENDENCIES = libev
+EF_LOOP_BIN_LICENSE = MIT
+EASYRUN_LICENSE_FILES = COPYING
+
+$(eval $(cmake-package))
